a4a806b34e03977dd4e19daa2887b43323da6ef5
AgroLogic <info@agrologic.net>
2025-06-17 13:28:19 +0200

    clean up program.cs for api

--- backend/ServiceDesk.Api/Program.cs
+++ backend/ServiceDesk.Api/Program.cs
@@ -1,5 +1,26 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Logging
+builder.Host.UseSerilog(((ctx, cfg) => cfg.ReadFrom.Configuration(ctx.Configuration)));
+
+// Services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
 app.Run();
